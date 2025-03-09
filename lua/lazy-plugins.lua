0eacdf974054673e7690a48543a24be418a8751d
Luclause <junjiet.ng97@gmail.com>
2025-03-10 10:16:20 +1100

    add: gitsigns, cmp, telescope

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -8,17 +8,17 @@ require('lazy').setup({
 
   require 'plugins/neo-tree',
 
-  -- require 'plugins/gitsigns',
+  require 'plugins/gitsigns',
 
   require 'plugins/which-key',
 
-  -- require 'plugins/telescope',
+  require 'plugins/telescope',
 
   -- require 'plugins/lspconfig',
 
   -- require 'plugins/conform',
 
-  --  require 'plugins/cmp',
+  require 'plugins/cmp',
  
   require 'plugins.catppuccinn',
 
