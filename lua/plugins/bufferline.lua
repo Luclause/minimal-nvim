2be125b0a93a59739cb7e853f635ba8e526de03d
Luclause <junjiet.ng97@gmail.com>
2025-03-11 01:30:06 +1100

    add: bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,4 +2,14 @@ return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("bufferline").setup {
+      options = {
+        mappings = {
+          { mode = "n", keys = "<Tab>", command = "BufferLineCycleNext" },
+          { mode = "n", keys = "<S-Tab>", command = "BufferLineCyclePrev" },
+        },
+      },
+    }
+  end,
 }
