587686153ccb5bf640721065adbc118bd05e3b97
Luclause <junjiet.ng97@gmail.com>
2025-03-10 11:31:29 +1100

    fix: catppuccinn-mocha theme

--- lua/plugins/catppuccinn.lua
+++ lua/plugins/catppuccinn.lua
@@ -1,55 +1,60 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  lazy = false,
-  ---@type CatppuccinOptions
-  opts = {
-    flavour = "macchiato",
-    transparent_background = false,
-    integrations = {
-      aerial = true,
-      alpha = true,
-      cmp = true,
-      dap = true,
-      dap_ui = true,
-      gitsigns = true,
-      illuminate = true,
-      indent_blankline = true,
-      markdown = true,
-      mason = true,
-      native_lsp = { enabled = true },
-      neotree = true,
-      notify = true,
-      semantic_tokens = true,
-      symbols_outline = true,
-      telescope = true,
-      treesitter = true,
-      ts_rainbow = false,
-      ufo = true,
-      which_key = true,
-      window_picker = true,
-      colorful_winsep = { enabled = true, color = "lavender" },
-    },
-  },
-  specs = {
-    {
-      "akinsho/bufferline.nvim",
-      optional = true,
-      opts = function(_, opts)
-        return require("astrocore").extend_tbl(opts, {
-          highlights = require("catppuccin.groups.integrations.bufferline").get(),
-        })
-      end,
-    },
-    {
-      "nvim-telescope/telescope.nvim",
-      optional = true,
-      opts = {
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "mocha",
       },
-    },
-  },
+      transparent_background = true, -- disables setting the background color.
+      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+      term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      no_italic = false, -- Force no italic
+      no_bold = false, -- Force no bold
+      no_underline = false, -- Force no underline
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      default_integrations = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        neotree = true,
+        treesitter = true,
+        telescope = true,
+        mini = { enabled = true, indentscope_color = "" },
+        mason = true,
+        alpha = true,
+        markdown = true,
+        which_key = true,
+        window_picker = true,
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      },
+    }
+
+    -- setup must be called before loading
+    vim.cmd.colorscheme "catppuccin"
+  end,
 }
